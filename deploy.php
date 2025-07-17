3fe8a44de4be69c341274c9a6a05db1e3bc208b3
mitantsoa1 <73829570+mitantsoa1@users.noreply.github.com>
2025-07-17 13:50:36 +0300

    add deployer

---
+++ deploy.php
@@ -0,0 +1,22 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony.php';
+
+// Config
+
+set('repository', 'https://github.com/mitantsoa1/chat-group.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('159.203.94.74')
+    ->set('remote_user', 'cetr-user')
+    ->set('deploy_path', '~/$PWD');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
